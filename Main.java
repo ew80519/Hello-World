4f58f14a3247af9e5c02400a384dcd9613952ea4
ew80519 <69615151+ew80519@users.noreply.github.com>
2020-08-13 12:47:20 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Ethan Wang");
+		System.out.println("I love music");
+        System.out.println("I want to major in computer science");
+        System.out.println("My dream college is UC Berkeley or Caltech");
+        System.out.println("I know Python but I don't know much Java");
+	}
+}
